a73b2e20a4d3d793f8eb5e79a832759c502f248c
Jaska <50119926+XzibitBlitz@users.noreply.github.com>
2023-01-11 13:57:14 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,27 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
   images: {
     loader: 'imgix',
     path: 'blitz-more-practice-tailwind.imgix.net',
   },
 }
+
+module.exports = nextConfig
